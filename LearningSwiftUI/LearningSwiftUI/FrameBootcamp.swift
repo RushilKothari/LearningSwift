edec8c7b8396e903d3bc7a0f0fd46315221ec242
RushilKothari <rushilkothari25.rk@gmail.com>
2023-07-04 21:07:01 +0530

    Added Frame Attributes

---
+++ LearningSwiftUI/LearningSwiftUI/FrameBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  FrameBootcamp.swift
+//  LearningSwiftUI
+//
+//  Created by Rushil Kothari on 04/07/23.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.red)
+//            .frame(width: 300, height: 300, alignment: .center)
+//            .frame(maxWidth: .infinity, maxHeight: .infinity ,alignment: .center)
+//            .background(Color.red)
+            .frame(height: 100, alignment: .top)
+            .background(Color.orange)
+            .frame(width: 150)
+            .background(Color.purple)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.pink)
+            .frame(height: 400)
+            .background(Color.green)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(Color.yellow)
+    }
+}
+
+struct FrameBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameBootcamp()
+    }
+}
